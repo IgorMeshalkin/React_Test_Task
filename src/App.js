507da00a9d3453b0359a2c7bb4290c566463d7bc
Igor Meshalkin <97287038+IgorMeshalkin@users.noreply.github.com>
2023-08-14 22:49:20 +0700

    Maked that websocket only works on main page.

--- src/App.js
+++ src/App.js
@@ -3,40 +3,15 @@ import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import StorePage from "./pages/StorePage";
 import Loader from "./ui/Loader/Loader";
-import React, {useEffect} from "react";
+import React from "react";
 import Navbar from "./componets/Navbar/Navbar";
-import {useDispatch, useSelector} from "react-redux";
-import {prepareEventForTable} from "./utils/eventsUtil";
-import {addToEventsList} from "./store/slices/eventsSlice";
-import {WEB_SOCKET_ADDRESS, WEB_SOCKET_DISCONNECT_MESSAGE} from "./properties/connectingProperties";
+import {useSelector} from "react-redux";
 
 function App() {
     //переменная содержащая адрес текущего роута.
     const location = useLocation();
     //состояние авторизации
     const authorization = useSelector(state => state.authorization.authorizationState)
-    //диспатч для вызова редюсера добавления события в список
-    const dispatch = useDispatch();
-    //редюсер добавления события в список
-    const addEventToList = (route) => dispatch(addToEventsList(route));
-
-    //если пользователь авторизован подключается к web socket и слушает, при появлении событий добавляет их в events
-    useEffect(() => {
-        if (authorization.isAuthorization) {
-            const ws = new WebSocket(WEB_SOCKET_ADDRESS);
-            ws.onmessage = event => {
-                addEventToList(prepareEventForTable(JSON.parse(event.data)))
-            };
-            ws.onclose = () => {
-                alert(WEB_SOCKET_DISCONNECT_MESSAGE);
-            };
-            return () => {
-                if (ws.readyState === 1) {
-                    ws.close();
-                }
-            };
-        }
-    }, [authorization])
 
     return (
         <>
--- src/componets/TablesForMainPage/EventsTable/EventsTable.jsx
+++ src/componets/TablesForMainPage/EventsTable/EventsTable.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './EventsTable.module.css'
 import {Table} from "antd";
-import {getEventsTableColumns} from "../../../utils/eventsUtil";
-import {useSelector} from "react-redux";
+import {getEventsTableColumns, prepareEventForTable} from "../../../utils/eventsUtil";
+import {useDispatch, useSelector} from "react-redux";
 import {EVENTS_TITLE} from "../../../properties/mainPageProperties";
+import {addToEventsList} from "../../../store/slices/eventsSlice";
+import {WEB_SOCKET_ADDRESS} from "../../../properties/connectingProperties";
 
 const EventsTable = () => {
-    //массив событий
+    // //массив событий
     const events = useSelector(state => state.eventsList.eventsList)
+    //состояние авторизации
+    const authorization = useSelector(state => state.authorization.authorizationState)
+    //диспатч для вызова редюсера добавления события в список
+    const dispatch = useDispatch();
+    //редюсер добавления события в список
+    const addEventToList = (route) => dispatch(addToEventsList(route));
+
+    //если пользователь авторизован подключается к web socket и слушает, при появлении событий добавляет их в events
+    useEffect(() => {
+        if (authorization.isAuthorization) {
+            const ws = new WebSocket(WEB_SOCKET_ADDRESS);
+            ws.onmessage = event => {
+                addEventToList(prepareEventForTable(JSON.parse(event.data)))
+            };
+            return () => {
+                if (ws.readyState === 1) {
+                    ws.close();
+                }
+            };
+        }
+    }, [authorization])
 
     return (
         <div className={styles.main}>
